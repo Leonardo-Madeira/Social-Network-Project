bea8685463a4e426d8ba247770e2b000086dbda6
Leonardo <leomadeiraa@hotmail.com>
2021-08-30 20:25:35 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
-	
 	session_start();// Esta iniciando uma sessão 
-
 	require('vendor/autoload.php');// comando do 'composer'
+
+	define('INCLUDE_PATH_STATIC','http://localhost/Projetos/redesocialdevweb20_/SpeakDev/Views/pages/');
 
 	$app = new SpeakDev\Application();
 
