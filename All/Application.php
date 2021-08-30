07de32d6332de01a46ade5af828654a81c727416
Leonardo <leomadeiraa@hotmail.com>
2021-08-29 23:57:50 -0300

    Update Application.php

--- All/Application.php
+++ All/Application.php
@@ -1,8 +1,31 @@
 <?php
+	namespace DankiCode;
+	class Application{
+		private $controller;
+		private function setApp(){
+			$loadName = 'DankiCode\Controllers\\';
+			$url = explode('/',@$_GET['url']);
 
-  class Application
-  {
-    
-  }
+			if($url[0] == ''){
+				$loadName.='Home';
+			}else{
+				$loadName.=ucfirst(strtolower($url[0]));
+			}
+
+			$loadName.='Controller';
+
+			if(file_exists($loadName.'.php')){
+				$this->controller = new $loadName();
+			}else{
+				die('Não existe essa página!');
+			}
+
+		}
+		public function run(){
+			$this->setApp();
+			$this->controller->index();
+		}
+		
+	}
 
 ?>
