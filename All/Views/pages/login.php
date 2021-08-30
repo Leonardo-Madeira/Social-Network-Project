bade6193d88b07910a34c850e8f7a1455a6c0b03
Leonardo <leomadeiraa@hotmail.com>
2021-08-29 23:57:43 -0300

    Create login.php

---
+++ All/Views/pages/login.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Login Rede Social</title>
+  <style>
+    *{margin: 0; padding: 0; box-sizing: border-box;}
+    html, body{height: 100%; background-color: black; color: white;}
+    .all{
+
+    }
+  </style>
+</head>
+<body>
+  <div class="all">
+    <div class="center">
+      <div class="text">
+        <h1>Speak Dev</h1>
+        <p>Social network template.</p>
+        <p>Connect with your friends and family, expand your learning with the social network Speak Dev</p>
+      </div>
+      <div class="form">
+        <form action="">
+        <input class="nome" type="text" name="Nome" placeholder="Name or Phone" />
+        <input type="password" name="Senha" placeholder="Password">
+        <button>Login</button>
+        <button>Create New Account</button>
+        </form>
+      </div>
+    </div>
+  </div>
+  <footer>
+    <h3>This project is open source and is available on my GitHub</h3>
+    <h4>Developed by Leonardo Madeira</h4>
+  </footer>
+</body>
+</html>
