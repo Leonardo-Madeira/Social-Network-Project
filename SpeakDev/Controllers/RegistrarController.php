e77ded92fb2ece4e45b88da4fa89d27e879c24af
Leonardo <leomadeiraa@hotmail.com>
2021-10-06 22:15:55 -0300

    Update RegistrarController.php

--- SpeakDev/Controllers/RegistrarController.php
+++ SpeakDev/Controllers/RegistrarController.php
@@ -7,28 +7,32 @@ class RegistrarController{
   public function index(){
 
     if(isset($_POST['registrar'])){
+      
       $name = $_POST['name'];
       $email = $_POST['email'];
       $password = $_POST['password'];
 
-      //if(filter_var($email,FILTER_VALIDATE_EMAIL)){ ESSA É UMA VALIDAÇÃO PARA VERIFICAR SE O EMAIL É VALIDO OU NÃO, EXTREMAMENTE UTIL.
-      //  \SpeakDev\Utilidades::alerta('Email inválido!');
-      //  \SpeakDev\Utilidades::redirect('http://localhost/Projetos/redesocialdevweb20_/registrar');
-      //}
-      if(strlen($password) < 7){
+      //E-MAIL ~ VALIDAÇÃO
+      if(!filter_var($email,FILTER_VALIDATE_EMAIL)){ //ESSA É UMA VALIDAÇÃO PARA VERIFICAR SE O EMAIL É VALIDO OU NÃO.
+      \SpeakDev\Utilidades::alerta('Email inválido!');
+       \SpeakDev\Utilidades::redirect('http://localhost/Projetos/redesocialdevweb20_/registrar');
+
+       //SENHA
+      }else if(strlen($password) < 7){
         \SpeakDev\Utilidades::alerta('Sua senha é muito curta, mínimo 8 caracteres.');
         \SpeakDev\Utilidades::redirect('http://localhost/Projetos/redesocialdevweb20_/registrar');
       }else{
+        // REGISTRAR USUÁRIO
         $password = \SpeakDev\Bcrypt::hash($password);
-        //$registro->execute(array($name,$email,$password));
-        \SpeakDev\Utilidades::alerta('Registrado com sucesso');
-        \SpeakDev\Utilidades::redirect(INCLUDE_PATH_STATIC.'login');
+        $registro = \SpeakDev\MySql::connect()->prepare("INSERT INTO usuarios VALUES (null,?,?,?)");
+        $registro->execute(array($name,$email,$password));
+
+        \SpeakDev\Utilidades::alerta('Registrado com sucesso meu querido(a)!');
+        \SpeakDev\Utilidades::redirect(INCLUDE_PATH_STATIC.'http://localhost/Projetos/redesocialdevweb20_/');
+        
       }
-
     }
-
       \SpeakDev\Views\MainView::render('registrar');
-      
   }
 }
 
